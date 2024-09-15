11cfea98ae9b59ade0b6f427d25a9b67572799f6
DellalMohamed <72273147+DellalMohamed@users.noreply.github.com>
2024-09-15 11:41:16 +0100

    ceate layout code

---
+++ src/components/global/AppLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="layout">
+    <v-layout>
+      <CardDrawer />
+      <v-main>
+        <slot></slot>
+      </v-main>
+      <AppNav />
+      <AppFooter />
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import AppNav from "../AppNav.vue";
+import CardDrawer from "../CardDrawer.vue";
+import AppFooter from "../AppFooter.vue";
+export default {
+  components: {
+    AppNav,
+    CardDrawer,
+    AppFooter,
+  },
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+//pinia
+import { createPinia } from "pinia";
+
+//mitt onfig
+import mitt from "mitt";
+const Emitter = mitt();
+//vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
+
+// Components
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+createApp(App)
+  .use(createPinia())
+  .provide("Emitter", Emitter)
+  .use(vuetify)
+  .use(router)
+  .mount("#app");
